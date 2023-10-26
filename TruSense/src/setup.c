903a0beae2f0f0e21b6417b1cfa089bad4cc5165
Shadow <80848587+DanTheHackerMan2@users.noreply.github.com>
2023-10-26 14:55:33 -0400

    Added setup file sets

---
+++ TruSense/src/setup.c
@@ -0,0 +1,8 @@
+/*
+ * setup.c
+ *
+ *  Created on: Oct 26, 2023
+ *      Author: Daniel
+ */
+
+
---
+++ TruSense/src/setup.h
@@ -0,0 +1,14 @@
+/*
+ * setup.h
+ *
+ *  Created on: Oct 26, 2023
+ *      Author: Daniel
+ *      Runs the setup for all external connections(sensors, firebase etc)
+ */
+
+#ifndef SETUP_H_
+#define SETUP_H_
+
+
+
+#endif /* SETUP_H_ */
