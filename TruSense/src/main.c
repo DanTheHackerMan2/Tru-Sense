aee1ea0e2e8360a953560adb27aa9ad2d9058a2c
Shadow <80848587+DanTheHackerMan2@users.noreply.github.com>
2023-10-26 13:51:03 -0400

    Fixing my project :)

---
+++ TruSense/src/main.c
@@ -0,0 +1,24 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 23, 2023
+ *      Author: Daniel
+ *      Example program for GPIO, Will continuously toggle GPIO24 (physical pin 18) at 500 ms rate
+ */
+#include<stdio.h>
+//#include <wiringPi.h>
+
+int main(int argc, char **argv) {
+	printf("Hello World");
+/*	const int led = 5;
+	wiringPiSetup();
+	pinMode(led,OUTPUT);
+	while(1){
+		digitalWrite(led, HIGH);
+		delay(500);
+		digitalWrite(led, LOW);
+		delay(500);
+	}
+	*/
+	return 0;
+}
