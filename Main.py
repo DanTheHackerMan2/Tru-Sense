a414341a137156b217ab4446b67d67342652ec1f
Shadow <80848587+DanTheHackerMan2@users.noreply.github.com>
2024-04-18 15:53:10 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ import RPi.GPIO as GPIO
 from adafruit_mcp3xxx.analog_in import AnalogIn
 import uuid
 
-user_uuid= uuid.uuid4() #user uuid generation. would be set in a config file for a set in stone use
+user_uuid= "c5382c3a-61eb-4133-b0dc-e188ff3553c4" #uuid.uuid4() #user uuid generation. would be set in a config file for a set in stone use
 spi = busio.SPI(clock=board.SCK, MISO=board.MISO, MOSI=board.MOSI)
 cs = digitalio.DigitalInOut(board.D5)
 mcp = MCP.MCP3008(spi, cs)
@@ -45,7 +45,7 @@ pers_data = {
     }
 
 loc_data = {
-        "Street1": "139 The Green",
+        "Street1": "139 The Green", 
         "Street2": "",
         "City": "Newark",
         "State": "DE",
@@ -54,20 +54,28 @@ loc_data = {
 
 db.child("Customers").child(user_uuid).child("Contact_Information").set(pers_data,user['idToken']) 
 db.child("Customers").child(user_uuid).child("Location").set(loc_data,user['idToken']) 
-
+#
+current_list = db.child("Customers").child(user_uuid).child("Devices").get().val() or []
+    # Update the entire list in the database
+db.child("Customers").child(user_uuid).child("Devices").set(current_list)
 #Set Up
-
-device_loc = { 
-        "Device_Location": "Kitchen",
-    }
-
+dev_loc="Kitchen"
+devLoc={
+    "Device Location": dev_loc
+}
+db.child("Customers").child(user_uuid).child("Devices").child(0).set(devLoc,user['idToken'])
 gas_con = False
 gas_r = channel0.value 
 temp_con = False
 temp_r = channel1.value 
 sensor_check=0
 
+
+
 while True:
+    
+
+    current_list = db.child("Customers").child(user_uuid).child("Devices").child(0).child("Readings").get().val() or []
     if(sensor_check>=100 or sensor_check== 0):
         if(gas_r != 0 ):
             gas_con=True
@@ -78,11 +86,12 @@ while True:
             "Temp Sensor": temp_con
         }   
         if(sensor_check==0):
-            db.child("Customers").child(user_uuid).child("Devices").child(device_loc).child("Sensors Active").set(sens_Data,user['idToken'])
-        db.child("Customers").child(user_uuid).child("Devices").child(device_loc).child("Sensors Active").update(sens_Data,user['idToken'])
+            db.child("Customers").child(user_uuid).child("Devices").child(0).child("Sensors Active").set(sens_Data,user['idToken'])
+        else:
+            db.child("Customers").child(user_uuid).child("Devices").child(0).child("Sensors Active").update(sens_Data,user['idToken'])
         sensor_check=1
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
+    
+    
     gas_r = channel0.value
     temp_r = channel0.value
 
@@ -92,9 +101,19 @@ while True:
     print('Raw Temp ADC Value: ', temp_r)
     print('ADC Voltage: ' + str(channel1.voltage) + 'V')
 
-    data = {
-        "Gas": (channel0.value-1601)
+
+
+
+        # Define a dictionary with the data you want to add, including a timestamp
+    data_to_add = {
+        "Gas": (channel0.value-1601),
+        "Timestamp": datetime.now().strftime("%H:%M:%S")
     }
-    db.child("Customers").child(user_uuid).child("Devices").child(device_loc).child("Readings").child(current_time).set(data,user['idToken'])
+
+    # Append the new data to the current list
+    current_list.append(data_to_add)
+
+    # Update the entire list in the database
+    db.child("Customers").child(user_uuid).child("Devices").child(0).child("Readings").set(current_list)
     sensor_check+=1
     time.sleep(5.0)#Limited to 20k samples a day, 85 k a seconds in a day. avg to 5
